fbe283506aab8b370e0540c69b8078f052bf6181
Marc Malliate <marc.malliate@gmail.com>
2022-02-21 12:43:33 +1030

    deploy

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
+import React from "react";
+import { useLocation, useHistory } from "react-router-dom";
 
 const Footer = () => {
   const location = useLocation();
@@ -7,26 +7,15 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
+        {location.pathname !== "/" && (
           <button
-            className="btn btn-dark mb-3"
+            className="btn btn-light mb-3"
             onClick={() => history.goBack()}
           >
             &larr; Go Back
           </button>
         )}
-        <h4>
-          Made with{' '}
-          <span
-            className="emoji"
-            role="img"
-            aria-label="heart"
-            aria-hidden="false"
-          >
-            ❤️
-          </span>{' '}
-          by Author.
-        </h4>
+        <h4>Remember, put your Big Rocks in first!</h4>
       </div>
     </footer>
   );
