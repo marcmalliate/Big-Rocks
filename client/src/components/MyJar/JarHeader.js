5152b65f13750f24752e6902ed53870ca4e5511f
Marc Malliate <marc.malliate@gmail.com>
2022-02-22 07:48:55 +1030

    header olour updated and image changed

--- client/src/components/MyJar/JarHeader.js
+++ client/src/components/MyJar/JarHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <span className="font-link">
-      <header className="bg-primary text-white py-2 px-3 text-center text-uppercase rounded mb-3">
+      <header className="bg-dark text-primary py-2 px-3 text-center text-uppercase rounded mb-3">
         <h3>My Jar</h3>
       </header>
     </span>
--- client/src/components/Rock/RockHeader.js
+++ client/src/components/Rock/RockHeader.js
@@ -4,7 +4,7 @@ import "./RockHeader.css";
 const RockHeader = () => {
   return (
     <span className="font-link">
-      <header className="bg-primary text-white py-3 px-6 text-center text-uppercase rounded mb-5">
+      <header className="bg-dark text-primary py-3 px-6 text-center text-uppercase rounded mb-5">
         <h3>My Big Rocks</h3>
       </header>
     </span>
